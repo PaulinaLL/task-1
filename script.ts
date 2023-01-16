11416abb561b509f2fa45e5536b68567cef59164
Paulina Lukarska <p.lukarska@Agenturs-MBP.fritz.box>
2023-01-16 19:54:26 +0100

    feat: rewrite to class

--- script.ts
+++ script.ts
@@ -1,39 +1,42 @@
-function addCustomStyle(
-  styleIdentifier: string,
-  selector: string,
-  attributes: any,
-  mediaQuery?: string
-): void {
-  const styleElement = <HTMLStyleElement>document.createElement("style");
-  document.body.appendChild(styleElement);
-  styleElement.setAttribute("id", styleIdentifier);
+class Styles {
+  public addCustomStyle(
+    styleIdentifier: string,
+    selector: string,
+    attributes: any,
+    mediaQuery?: string
+  ): void {
+    const styleElement = <HTMLStyleElement>document.createElement("style");
+    document.body.appendChild(styleElement);
+    styleElement.setAttribute("id", styleIdentifier);
 
-  const selectorExsistInDocument = document.querySelectorAll(selector).length
-    ? true
-    : false;
+    const selectorExsistInDocument = document.querySelectorAll(selector).length
+      ? true
+      : false;
 
-  if (mediaQuery) {
-    if (selectorExsistInDocument && selector && attributes) {
-      styleElement.innerHTML =
-        "@media (" +
-        mediaQuery +
-        ") " +
-        " { " +
-        selector +
-        " " +
-        attributes +
-        " } ";
-    }
-  } else {
-    if (selectorExsistInDocument && selector && attributes) {
-      styleElement.innerHTML = selector + " " + attributes;
+    if (mediaQuery) {
+      if (selectorExsistInDocument && selector && attributes) {
+        styleElement.innerHTML =
+          "@media (" +
+          mediaQuery +
+          ") " +
+          " { " +
+          selector +
+          " " +
+          attributes +
+          " } ";
+      }
+    } else {
+      if (selectorExsistInDocument && selector && attributes) {
+        styleElement.innerHTML = selector + " " + attributes;
+      }
     }
   }
 }
 
-addCustomStyle(
+const instance = new Styles();
+instance.addCustomStyle(
   "myStyle",
   ".someClassName",
-  "{ background-color: blue }",
+  "{background-color: yellow }",
   "max-width: 909px"
 );
